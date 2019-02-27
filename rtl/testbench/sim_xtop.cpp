0d55c894e0de436e976f2de47121d490f5dba5c9
Joao Ratinho <joao.ratinho@tecnico.ulisboa.pt>
2019-02-27 17:27:21 +0000

    Verilator simulation now finishes, testbench still needs to be improved

--- rtl/testbench/sim_xtop.cpp
+++ rtl/testbench/sim_xtop.cpp
@@ -23,19 +23,22 @@ int main(int argc, char** argv, char** env) {
   int t = 0;
 
   while (!Verilated::gotFinish()) {
-    if (t == 20)
+    if (t == 10 || t == 15)
       top->rst = 1;
     else 
       top->rst = 0;
-    if (t == 70) {
+    if (t == 60 || t == 65) {
       top->par_addr = 0;
       top->par_we = 1;
       top->par_in = 1; //must be non-zero to jump to main program
     }
-    if (t == 80) {
+    if (t == 70 || t ==75) {
       top->par_we = 0;
       top->par_addr = 0;
     }
+    //while (par_out != 0) t += 10;
+    
+    if (t > 80 && top->par_out == 0) Verilated::gotFinish(true);
 
     top->clk = !top->clk;
     top->eval();
@@ -46,9 +49,6 @@ int main(int argc, char** argv, char** env) {
 
 
   }
-
-
-
 
   tfp->close();
   top->final();
