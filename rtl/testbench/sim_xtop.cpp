bce2259c64c7d6317d00ced55d0880a317a1fd34
Joao Ratinho <joao.ratinho@tecnico.ulisboa.pt>
2019-02-27 13:11:10 +0000

    Added DEBUG option to verilator makefile

--- rtl/testbench/sim_xtop.cpp
+++ rtl/testbench/sim_xtop.cpp
@@ -10,8 +10,46 @@ int main(int argc, char** argv, char** env) {
   
   top->trace (tfp, 99);
   tfp->open ("Sim.vcd");
-  while (!Verilated::gotFinish()) { top->eval();
+  
+  //Initialize inputs
+  top->clk = 1;
+  top->rst = 0;
+  
+  //Initialize parallel interface
+  top->par_addr = 0;
+  top->par_we = 0;
+  top->par_in = 0;
+
+  int t = 0;
+
+  while (!Verilated::gotFinish()) {
+    if (t == 20)
+      top->rst = 1;
+    else 
+      top->rst = 0;
+    if (t == 70) {
+      top->par_addr = 0;
+      top->par_we = 1;
+      top->par_in = 1; //must be non-zero to jump to main program
+    }
+    if (t == 80) {
+      top->par_we = 0;
+      top->par_addr = 0;
+    }
+
+    top->clk = !top->clk;
+    top->eval();
+    tfp->dump(t);
+    
+    //Clock period / 2
+    t += 5;
+
+
   }
+
+
+
+
   tfp->close();
   top->final();
   delete top;
