7bdcadc8e4897a6fc94f75ab65a7f64cb69333b9
Joao Ratinho <joao.ratinho@tecnico.ulisboa.pt>
2019-03-01 15:42:37 +0000

    Improved verilator testbench

--- rtl/testbench/sim_xtop.cpp
+++ rtl/testbench/sim_xtop.cpp
@@ -20,35 +20,41 @@ int main(int argc, char** argv, char** env) {
   top->par_we = 0;
   top->par_in = 0;
 
-  int t = 0;
+  int t, start_time, k = 0;
+  int clk_period = 10;
+  int r0;
+  
 
   while (!Verilated::gotFinish()) {
-    if (t == 10 || t == 15)
+    if (t == 2*clk_period || t == 3*clk_period)
       top->rst = 1;
     else 
       top->rst = 0;
-    if (t == 60 || t == 65) {
+    if (t == 12*clk_period || t == 13*clk_period) {
       top->par_addr = 0;
       top->par_we = 1;
       top->par_in = 1; //must be non-zero to jump to main program
+      start_time = t;
     }
-    if (t == 70 || t ==75) {
+    if (t == 14*clk_period || t == 15*clk_period) {
       top->par_we = 0;
       top->par_addr = 0;
     }
-    //while (par_out != 0) t += 10;
     
-    if (t > 80 && top->par_out == 0) Verilated::gotFinish(true);
+    if (t > 16*clk_period && top->par_out == 0) Verilated::gotFinish(true);
 
     top->clk = !top->clk;
     top->eval();
     tfp->dump(t);
     
     //Clock period / 2
-    t += 5;
+    t += clk_period/2;
 
 
   }
+
+  printf("Execution time in clock cycles: %0d \n", (t-start_time)/10);
+
 
   tfp->close();
   top->final();
