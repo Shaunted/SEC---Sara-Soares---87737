db2564155a9d833dd41c81fdf13c19b4169037c7
Shaunted <shaunted@DESKTOP-QSISSI8.localdomain>
2020-01-08 12:30:23 +0000

    Project files updated

--- Lab_Project/verilog/timer.v
+++ Lab_Project/verilog/timer.v
@@ -15,57 +15,85 @@ module timer(
         output reg [3:0] min0,
         //cpu interface
         input sel,
-        input [5:0] data_in,
-        output [5:0] data_out,
+        input [3:0] data_in,
+        output [3:0] data_out,
         input  address,
         input write_en
 );
 
 reg [25:0] c;
+reg [5:0] r;
 reg [5:0] s;
 reg [3:0] m;
 reg [1:0] h;
 
 always @(posedge clk) begin
-    if (rst) begin
-        s <= 0;
-        m <= 0;
-        h <= 0;
-        c <= 0;
-    end else begin
-        if(c == 50000000 - 1) begin
-            c <= 0;
-        end
-        else begin
-            c <=  c + 1;
-        end
+	if (rst) begin
+		s <= 0;
+		min0 <= 0;
+		min1<= 0;
+		hour0<= 0;
+		hour1<= 0;
+		c <= 0;
+	end 
+	else begin
+		if(c == 50000000 - 1) begin
+			c <= 0;
+		end
+      else begin
+			c <=  c + 1;
+      end
+	end
+	if (c == 50000000 - 1) begin
+		s <= s + 1;
+		if (s == 59) begin
+			s <= 0;
+      end
+	end
 
-        if (c == 50000000 - 1) begin
-            s <= s + 1;
-            if (s == 59) begin
-                s <= 0;
-            end
-        end
+	if ( s == 59) begin
+		min0 <= min0 + 1;
+		if ( min0 == 9) begin
+			min0 <= 0;
+        	min1 <= min1 + 1;
+			if (min1 == 5) begin
+				min1 <= 0;
+			end
+		end
+	end
 
-        if ( s == 59) begin
-            m <= m + 1;
-            if ( m == 9) begin
-                time[3:0] <= 0;
-                time[6:0] <= time[6:0] + 1;
-                if (time[6:0] == 5)
-                    time[6:0] <= 0;
-            end
-        end
+	if (min1==5 && min0==9) begin
+		hour0 <= hour0 + 1;
+		if ( hour0 == 9) begin
+			hour0 <= 0;
+			hour1 <= hour1 + 1;
+			if(hour1 == 2 && hour0 == 3)begin
+				hour0 <= 0;
+				hour1 <= 0;
+			end
+		end    
+	end
 
-        if (time[6:0]==5 && time[3:0]==9) begin
-            h <= h + 1;
-            if ( h == 3) begin
-                time[10:7] <= 0;
-                time[12:11] <= time[12:11] + 1;
-                if(time[12:11] == 2)
-                    time[12:11] <= 0;
-            end    
-        end
+    if(write_en) begin
+        if(sel) begin
+            if(address == MIN0)
+            	data_out <= min0;
+			if(address == MIN1)
+            	data_out <= {0,min1};
+			if(address == HOUR0)
+            	data_out <= hour0; 
+			if(address == HOUR1)
+            	data_out <= {0,0,hour1};
+		end
+		if(address == MIN0)
+            min0 <= data_in;
+		if(address == MIN1)
+            min1 <= data_in;
+		if(address == HOUR0)
+            hour0 <= data_in; 
+		if(address == HOUR1)
+            hour1 <= data_in;
     end
+
 end
 endmodule
